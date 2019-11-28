cb51f98a74455985ba02a15c03e97ae459ce6687
Dia Abdoul <dia.abdoul@gmail.com>
2019-11-28 09:11:18 -0500

    QuizClass1

--- src/Quiz.js
+++ src/Quiz.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 let quizData = require('./quiz_data.json');
 
 class Quiz extends Component{
-    
+
     constructor(props){
         super(props);
         this.state={quiz_position:1};
@@ -18,4 +18,4 @@ class Quiz extends Component{
     }
 }
 
-export default  Quiz;
+export default Quiz
